ef9af90f5afd5e21749d0ceb2198180a6b35abf6
Laura Vuorenoja <laura.vuorenoja@gmail.com>
2022-01-22 18:47:05 +0200

    Remove hover effect from quiz view.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@ module.exports = {
   purge: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      screens: {
-        "hover-hover": { raw: "(hover: hover)" }
-      }
-    }
+    extend: {}
   },
   variants: {
     extend: {}
