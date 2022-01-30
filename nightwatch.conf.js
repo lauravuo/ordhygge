a5b657d16c5a10a1302ffe0b74061caa7677f5e3
Laura Vuorenoja <laura.vuorenoja@gmail.com>
2022-01-30 08:08:10 +0200

    Update app info.

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -100,7 +100,7 @@ module.exports = {
             //'--no-sandbox',
             //'--ignore-certificate-errors',
             //'--allow-insecure-localhost',
-            //'--headless'
+            "--headless"
           ]
         }
       },
